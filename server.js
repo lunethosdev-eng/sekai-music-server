92fbb71b4e5b5bd096b3eae426afb4a1b5897540
lunethosdev-eng <lunethos.dev@gmail.com>
2026-09-23 21:10:39 -0600

    Update server.js

--- server.js
+++ server.js
@@ -508,7 +508,7 @@ async function runSlowScraper(maxSongsTarget = 1000, customCategory = null) {
         }
     } catch (e) {
         console.error('Error general en scraper:', e.message);
-    } fontinally {
+    } finally {
         isScraperRunning = false;
         scraperCancelRequested = false;
     }
@@ -987,4 +987,3 @@ app.listen(PORT, async () => {
         }
     }
 });
-
